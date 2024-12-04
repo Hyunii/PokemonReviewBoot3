51b129d05a419acc9e94f73ee5a6c7a08269c0c8
KWANG-HYUN <kuncheck@gmail.com>
2024-12-04 17:07:48 +0900

    add

--- src/main/java/com/pokemonreview/api/exceptions/advice/GlobalExceptionHandler.java
+++ src/main/java/com/pokemonreview/api/exceptions/advice/GlobalExceptionHandler.java
@@ -14,6 +14,20 @@ import java.time.Instant;
 @RestControllerAdvice
 @Slf4j
 public class GlobalExceptionHandler {
+
+    /*
+    Spring 6 버전에 추가된 ProblemDetail 객체 에러정보를 담아서 리턴하는 방법
+     */
+//    @ExceptionHandler(MyResourceException.class)
+//    protected ProblemDetail handleException(MyResourceException ex) {
+//        ProblemDetail problemDetail = ProblemDetail.forStatus(ex.getStatusCode());
+//        problemDetail.seTitle("Not Found");
+//        problemDetail.setDetail(ex.getMessage());
+//        problemDetail.setProperty("errorCategory", "Generic");
+//        problemDetail.setProperty("timestamp", Instant.now());
+//        return problemDetail;
+//    }
+
     @ExceptionHandler(MyResourceException.class)
     public ResponseEntity<ErrorObject> handleResourceNotFoundException(MyResourceException ex) {
         ErrorObject errorObject = new ErrorObject();
